841093bb106e8c7fd34bc6dbaa7d924ee7ca5dba
Arthur Rodrigues <rodriguesarthur7777@gmail.com>
2024-02-11 17:58:57 -0300

    feat: Implementing validator

---
+++ validator_raw.py
@@ -0,0 +1,39 @@
+from cerberus import Validator
+
+body = {
+    "data": {
+        "elemento1": 123,
+        "elemento2": "olaMundo",
+        "elemento3": "123"
+    }
+}
+
+body_validator = Validator({
+    "data": {
+        "type": "dict",
+        "schema": {
+            "elemento1": {
+                "type": "float",
+                "required": True,
+                "empty": False
+            },
+            "elemento2": {
+                "type": "string",
+                "required": True,
+                "empty": True
+            },
+            "elemento3": {
+                "type": "string",
+                "required": True,
+                "empty": False
+            }
+        }
+    }
+})
+response = body_validator.validate(body)
+
+if response:
+    print("Body validado com sucesso")
+else:
+    print(body_validator.errors)
+    print("Body invalido")
